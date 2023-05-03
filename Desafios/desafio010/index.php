1db03295ff2693befdf10d84ac2ddc0011f011c5
andregu14 <andregustavo147@gmail.com>
2023-05-02 23:10:14 -0300

    Update index.php

--- Desafios/desafio010/index.php
+++ Desafios/desafio010/index.php
@@ -11,26 +11,23 @@
     <?php 
         $ano = $_REQUEST['ano'] ?? 2000;
         $ano2 = $_REQUEST['ano2'] ?? date("Y");
-        $ano = intval($ano);
-    if ($ano > $ano2):
-        $ano = $ano2;
-    endif;
-
+        // calcula anos
+        $valor = intval($ano2) - intval($ano);
     ?>
     <main>
         <h2>Calculando a sua idade</h2>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
         <label for="ano">Em que ano você nasceu?</label>
-        <input type="number" name="ano" id="ano" value="<?=$ano?>">
+        <input type="number" name="ano" id="ano" value="<?=$ano?>" required min="1" max="<?= date("Y") - 1?>">
         <label for="ano2">Quer saber sua idade em que ano?(atualmente estamos em <strong><?=date("Y")?></strong>)</label>
-        <input type="number" name="ano2" id="ano2" value="<?=$ano2?>">
+        <input type="number" name="ano2" id="ano2" value="<?=$ano2?>" required min="1">
         <input type="submit" value="Qual será minha idade?">
         </form>
 
     </main>
     <section>
         <h2>Resultado</h2>
-        <p>Quem nasceu em <?=$ano?> vai ter <strong><?=$ano2 - $ano?> anos</strong> em <?=$ano2?>!
+        <p>Quem nasceu em <?=$ano?> tera <strong><?=$valor?> anos</strong> no ano <?=$ano2?>!
         
         </p>
     </section>
