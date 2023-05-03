cafcbcd63a115bc0ce970fb8768fab005779a68a
andregu14 <andregustavo147@gmail.com>
2023-05-02 23:26:18 -0300

    Update index.php

--- Desafios/desafio010/index.php
+++ Desafios/desafio010/index.php
@@ -9,18 +9,21 @@
 </head>
 <body>
     <?php 
-        $ano = $_REQUEST['ano'] ?? 2000;
+        $ano = $_REQUEST['ano'] ?? '2000';
         $ano2 = $_REQUEST['ano2'] ?? date("Y");
         // calcula anos
         $valor = intval($ano2) - intval($ano);
     ?>
     <main>
         <h2>Calculando a sua idade</h2>
-        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+        
         <label for="ano">Em que ano você nasceu?</label>
         <input type="number" name="ano" id="ano" value="<?=$ano?>" required min="1" max="<?= date("Y") - 1?>">
+
         <label for="ano2">Quer saber sua idade em que ano?(atualmente estamos em <strong><?=date("Y")?></strong>)</label>
         <input type="number" name="ano2" id="ano2" value="<?=$ano2?>" required min="1">
+
         <input type="submit" value="Qual será minha idade?">
         </form>
 
